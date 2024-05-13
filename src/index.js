54ef0b3b38f1e2318499d98e1a6947d6fdca1fbf
Alex <alexvnep@gmail.com>
2024-05-13 14:59:13 +0200

    Add relevent imports and call menuButton and closeMenu Handlers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import { toggleDropDown } from "./dropDownMenuScript.js";
+import { menuButtonHandler } from "./dropDownMenuScript.js";
+import { closeMenuHandler } from "./dropDownMenuScript.js";
 import "./style.css";
+
+menuButtonHandler();
+closeMenuHandler();
