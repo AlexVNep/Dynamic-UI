8ce07bc8566284e84849b466fd853745eeaf7cce
Alex <alexvnep@gmail.com>
2024-05-13 14:35:48 +0200

    Add updated comments explaining function and eventListener

--- src/dropDownMenuScript.js
+++ src/dropDownMenuScript.js
@@ -1,17 +1,15 @@
 const menuButton = document.querySelector(".menuBtn");
 const dropDownMenu = document.querySelector(".dropdown");
 
-// Add event listener to menu button that shows menu items when clicked
-// when men button is clicked a new class of visible is added to the menu items
-// Menu items must all have same class name
-
+//Adds the class 'show' to dropDownMenu
 function toggleDropDown() {
   dropDownMenu.classList.toggle("show");
 }
 
+//Listens for the click to execute the toggleDropDown function
 menuButton.addEventListener("click", function (e) {
   e.stopPropagation();
   toggleDropDown();
 });
 
-// export { toggleDropDown };
+export { toggleDropDown };
